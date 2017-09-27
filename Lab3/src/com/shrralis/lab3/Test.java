e6aba4ce95f06d28937684b17b7b6cdb469624ed
shrralis <shrralis@gmail.com>
2017-09-27 20:09:31 +0300

    Deleted unnecessary Test class.

--- Lab3/src/com/shrralis/lab3/Test.java
+++
@@ -1,9 +0,0 @@
-package com.shrralis.lab3;
-
-import com.shrralis.lab3.interfaces.ISquare;
-
-public class Test {
-    public static void main(String[] args) {
-        ISquare s = Square.
-    }
-}
